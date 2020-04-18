2c810604ad491b6315414547dca6c7ada56e7b55
Aleksa <aleksabudrak@gmail.com>
2020-04-18 15:23:06 +0200

    Dodavanje.

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = ({onInputChange ,onButtonSubmit}) => {
+    return (
+    <div>
+        <div>
+            <p className='f3'>
+            {'Ubaci sliku da detektujemo lice'}
+            </p>
+        </div>
+        <div className= 'center'>
+            <div className='form center pa4 br3 shadow-5'>
+                <input className='f4 pa2 w-60 center' type="tex" onChange={onInputChange}  />
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue' onClick={onButtonSubmit}>Detektuj!</button>
+            </div>
+        </div>
+    </div>
+        
+    )
+
+}
+
+export default ImageLinkForm;
